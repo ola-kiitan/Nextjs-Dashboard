885ef45fe3784093a2d7e7b9a4dff5b146e5a16f
Olayinka Oginni <olayinka.oginni@idealo.de>
2024-04-18 16:05:25 +0200

    first commit

---
+++ src/app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+const Customers = () => {
+ return (
+  <div>
+   <h1>Customers</h1>
+   {/* Your customer list goes here */}
+  </div>
+ );
+};
+
+export default Customers;
---
+++ src/app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Invoices: React.FC = () => {
+ return (
+  <div>
+   <h1>Invoices</h1>
+   {/* Add your content here */}
+  </div>
+ );
+};
+
+export default Invoices;
